f86a8d71a9dbdefebfe35b08e3a852cb047069c0
lidya dagnew <lidyadagnew7@gmail.com>
2024-02-06 16:12:57 -0500

    change

--- music_backend/config/db.js
+++ music_backend/config/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://lidu:lidu1234@cluster0.h4bxhba.mongodb.net/?retryWrites=true&w=majority');
-    // const conn = await mongoose.connect(process.env.MONGO_URI);
+    // const conn = await mongoose.connect('mongodb+srv://lidu:lidu1234@cluster0.h4bxhba.mongodb.net/?retryWrites=true&w=majority');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`Mongo DB connected : ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
