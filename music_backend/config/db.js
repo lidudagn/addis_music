c4e75759d78666fb78388010399ac927bb535e45
lidya dagnew <lidyadagnew7@gmail.com>
2024-02-06 16:31:27 -0500

    change

--- music_backend/config/db.js
+++ music_backend/config/db.js
@@ -16,7 +16,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://lidu:lidu1234@cluster0.h4bxhba.mongodb.net/mydatabase?retryWrites=true&w=majority', {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
