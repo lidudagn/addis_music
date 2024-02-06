45105ffba3fa6cf9012579491d998dd9409e643d
lidya dagnew <lidyadagnew7@gmail.com>
2024-02-06 16:38:17 -0500

    change

--- music_backend/config/db.js
+++ music_backend/config/db.js
@@ -1,28 +1,12 @@
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//     // const conn = await mongoose.connect('mongodb+srv://lidu:lidu1234@cluster0.h4bxhba.mongodb.net/?retryWrites=true&w=majority');
-//     const conn = await mongoose.connect(process.env.MONGO_URI);
-//     console.log(`Mongo DB connected : ${conn.connection.host}`);
-//   } catch (error) {
-//     console.log(error);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    // const conn = await mongoose.connect('mongodb+srv://lidu:lidu1234@cluster0.h4bxhba.mongodb.net/?retryWrites=true&w=majority');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Mongo DB connected : ${conn.connection.host}`);
   } catch (error) {
-    console.error(error);
+    console.log(error);
     process.exit(1);
   }
 };
