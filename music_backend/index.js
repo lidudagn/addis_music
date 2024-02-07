988412a860f4826e57202d68525032f758f212bd
lidya dagnew <lidyadagnew7@gmail.com>
2024-02-07 07:43:37 -0500

    change

--- music_backend/index.js
+++ music_backend/index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-  origin: ["https://addis-music-project.vercel.app"],
+  origin: "https://addis-music-project.vercel.app",
   credentials: true, //acces
 };
 
